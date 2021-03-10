943bd3833eb9811bfec4c347b7e6834dd4e5e545
klajdi44 <klajdi1996@yahoo.gr>
2021-03-10 17:58:26 +0100

    added detailed page

--- src/Components/cardItem/CardItem.tsx
+++ src/Components/cardItem/CardItem.tsx
@@ -1,59 +1,58 @@
 import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import fetchData from '../../modules/fetchData';
 import Loader from '../loader/LoaderComponent';
 
 function CardItem(props: any): JSX.Element {
-  console.log('hi');
-  type RootObject = {
-    meals: Meal[];
-  };
+  // type RootObject = {
+  //   meals: Meal[];
+  // };
 
   type Meal = {
-    idMeal: string;
-    strMeal: string;
+    idMeal: string | null;
+    strMeal: string | null;
     strDrinkAlternate?: any;
-    strCategory: string;
-    strArea: string;
-    strInstructions: string;
-    strMealThumb: string;
-    strTags: string;
-    strYoutube: string;
-    strIngredient1?: string;
-    strIngredient2?: string;
-    strIngredient3?: string;
-    strIngredient4?: string;
-    strIngredient5?: string;
-    strIngredient6?: string;
-    strIngredient7?: string;
-    strIngredient8?: string;
-    strIngredient9?: string;
-    strIngredient10?: string;
-    strIngredient11?: string;
-    strIngredient12?: string;
-    strIngredient13?: string;
-    strIngredient14?: string;
-    strIngredient15?: string;
+    strCategory: string | null;
+    strArea: string | null;
+    strInstructions: string | null;
+    strMealThumb: string | null;
+    strTags: string | null;
+    strYoutube: string | null;
+    strIngredient1?: string | null;
+    strIngredient2?: string | null;
+    strIngredient3?: string | null;
+    strIngredient4?: string | null;
+    strIngredient5?: string | null;
+    strIngredient6?: string | null;
+    strIngredient7?: string | null;
+    strIngredient8?: string | null;
+    strIngredient9?: string | null;
+    strIngredient10?: string | null;
+    strIngredient11?: string | null;
+    strIngredient12?: string | null;
+    strIngredient13?: string | null;
+    strIngredient14?: string | null;
+    strIngredient15?: string | null;
     strIngredient16?: any;
     strIngredient17?: any;
     strIngredient18?: any;
     strIngredient19?: any;
     strIngredient20?: any;
-    strMeasure1: string;
-    strMeasure2: string;
-    strMeasure3: string;
-    strMeasure4: string;
-    strMeasure5: string;
-    strMeasure6: string;
-    strMeasure7: string;
-    strMeasure8: string;
-    strMeasure9: string;
-    strMeasure10: string;
-    strMeasure11: string;
-    strMeasure12: string;
-    strMeasure13: string;
-    strMeasure14: string;
-    strMeasure15: string;
+    strMeasure1: string | null;
+    strMeasure2: string | null;
+    strMeasure3: string | null;
+    strMeasure4: string | null;
+    strMeasure5: string | null;
+    strMeasure6: string | null;
+    strMeasure7: string | null;
+    strMeasure8: string | null;
+    strMeasure9: string | null;
+    strMeasure10: string | null;
+    strMeasure11: string | null;
+    strMeasure12: string | null;
+    strMeasure13: string | null;
+    strMeasure14: string | null;
+    strMeasure15: string | null;
     strMeasure16?: any;
     strMeasure17?: any;
     strMeasure18?: any;
@@ -72,6 +71,25 @@ function CardItem(props: any): JSX.Element {
     `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
   );
 
+  const ingredients = [];
+  let filteredIgredients = [];
+  const meal: any = data && data?.meals[0];
+
+  //loop through the igredients and the measures and return them together
+  if (meal) {
+    for (let i = 1; i <= 20; i++) {
+      let singleIngredient =
+        meal[`strMeasure${i}`] + ' ' + meal[`strIngredient${i}`];
+      ingredients.push(singleIngredient);
+    }
+  }
+
+  if (ingredients.length > 0) {
+    filteredIgredients = ingredients.filter(
+      el => el !== 'null null' && el != ' '
+    );
+    console.log(filteredIgredients);
+  }
   return (
     <>
       {loadingState === 'idle' && <Loader />}
@@ -82,10 +100,33 @@ function CardItem(props: any): JSX.Element {
         </div>
       )}
       {data && (
-        <div style={{ color: '#000' }}>
-          {data.meals[0].strMeal}
-          <div style={{ color: '#000' }}>this is the {id}</div>
-        </div>
+        <section className='card-item-wrapper'>
+          <article className='back-button-wrapper'>
+            <Link to='/'>
+              <i className='fas fa-chevron-left'></i>
+            </Link>
+          </article>
+
+          <article className='card-item'>
+            <div className='wrapper-fix'>
+              <div className='img-section'>
+                <img src={meal.strMealThumb} alt='dish' />
+                <h1>{meal.strMeal}</h1>
+              </div>
+
+              <div className='category-area'>
+                <span>{meal.strCategory}</span>
+                <span>{meal.strArea}</span>
+              </div>
+
+               <div className='tags'>
+                {meal?.strTags?.split(',').map((el: string | null) => (
+                  <span>#{el}</span>
+                ))}
+              </div>
+            </div>
+          </article>
+        </section>
       )}
     </>
   );
