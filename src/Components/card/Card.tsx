f391a51532e23d26c52ddbfd0cc465e8778b3598
klajdi44 <klajdi1996@yahoo.gr>
2021-03-11 14:03:59 +0100

    testing lazy loading for images

--- src/Components/card/Card.tsx
+++ src/Components/card/Card.tsx
@@ -15,13 +15,13 @@ export default function Card(props: Props): JSX.Element {
   useEffect(() => {
     gsap.fromTo(
       '.card',
-      { opacity: 0, y: 100 + '%' },
+      { opacity: 0.1, y: 100 + '%' },
       {
         opacity: 1,
         y: 0 + '%',
-        stagger: 0.1,
+        stagger: 0.01,
         duration: 1,
-        delay: 0.3,
+
         ease: 'back.out(1.3)',
       }
     );
@@ -33,7 +33,13 @@ export default function Card(props: Props): JSX.Element {
         return (
           <Link to={`/CardItem/${meal.idMeal}`} key={meal.strMeal}>
             <article className='card'>
-              <img src={meal.strMealThumb} alt='dish' />
+              <img
+                loading='lazy'
+                src={meal.strMealThumb}
+                alt='dish'
+                width='200'
+                height='200'
+              />
               <h3>{meal.strMeal}</h3>
             </article>
           </Link>
